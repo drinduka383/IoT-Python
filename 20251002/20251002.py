602ce82682d6e440873892bb7ff8aa2a762b9d42
drinduka383 <drin.duka@gmail.com>
2025-10-02 15:21:25 +0200

    20251002 - 15:21

--- 20251002/20251002.py
+++ 20251002/20251002.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+# Data types
+
+# Explained int, unsigned int, among other datatypes.
+
+# Basic login authentication
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "IoT":
+    if password == "12345678":
+        print("Access granted!")
+    else:
+        print("Wrong password!")
+else:
+    print("Wrong username or password!")
